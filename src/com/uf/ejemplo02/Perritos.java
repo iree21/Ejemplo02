7332d804793bd83d6098a0642a7b7d60197d1fed
irene <irenealonsocordero@gmail.com>
2022-10-06 20:42:02 +0200

    Commit inicial, esta vez si

---
+++ src/com/uf/ejemplo02/Perritos.java
@@ -0,0 +1,13 @@
+package com.uf.ejemplo02;
+
+public class Perritos {
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    private String raza;
+}
